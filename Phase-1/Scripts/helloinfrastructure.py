b5404cb39af2eca2238e7ab1f7ebeb946d4cc28e
higgidv <higgins.dasean@gmail.com>
2026-07-28 14:26:44 -0700

    Add first Python Infrastructure Script

---
+++ Phase-1/Scripts/helloinfrastructure.py
@@ -0,0 +1,7 @@
+# First infrastructure Python script
+
+engineername = "DaSean"
+currentrole = "IT support"
+targetrole = "Linux infrastructure engineer"
+
+print(f"{engineername} is currently in {currentrole} and is working toward becoming a {targetrole}.")
